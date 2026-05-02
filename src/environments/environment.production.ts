592311e2f4623c22ee33baf8fc07edf773f7cc00
Alejandro Ibanez <coder.alejandro@gmail.com>
2026-05-02 01:03:42 -0300

    feat: actualizo url

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://gymappmanagement-ckg5chcyctdacdf9.brazilsouth-01.azurewebsites.net/api'
+  apiUrl: 'https://gymappmanagement-ckg5chcyctdacdf9.brazilsouth-01.azurewebsites.net'
 };
